384bd609df20ccafcf9b6477e28ff22fb4f8f931
RK <rk@whitehatglobal.org>
2022-07-23 18:08:55 +0530

    clean

--- sga_chits/sga_chits/doctype/area/area.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, White Hat Global and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class Area(Document):
-	pass
--- sga_chits/sga_chits/doctype/area/test_area.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, White Hat Global and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestArea(unittest.TestCase):
-	pass
